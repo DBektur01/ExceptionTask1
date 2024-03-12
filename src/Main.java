acfe34b84d0fdce87f7b8b7867184d2cc52f8789
User h <bektur.duyshenbekuulu@iaau.edu.kg>
2024-03-12 17:09:00 +0600

    Exception12022024

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,63 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.LocalDate;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String name = " ";
+        String lastName = " ";
+        int dateOfBirth = 0;
+        int  yearStart = 0;
+
+            try {
+                System.out.println("Атынызды жазыныз:");
+                name = scanner.nextLine();
+
+                if (name.matches(".*\\d.*")) {
+                    throw new InputMismatchException("Адамдын аты сандан турбайт");
+
+                }
+
+                System.out.println("Фамилиянызды жазыныз: ");
+                lastName = scanner.nextLine();
+                if (lastName.matches(".*\\d.*")) {
+                    throw new InputMismatchException("Адамдын фамилиясы сан болбойт");
+
+                }
+                System.out.println("Туулган жылынызды жазыныз: ");
+                dateOfBirth = scanner.nextInt();
+
+                if (!String.valueOf(dateOfBirth).matches(".*\\d.*")) {
+                    throw new InputMismatchException("dateOfBirth  тамга болбойт");
+                }
+                if (dateOfBirth > LocalDate.now().getYear()) {
+                    throw new InputMismatchException("dateOfBirth 2024жылдан чон болбойт ");
+                }
+                System.out.println("Жумушка кирген жылынызды жазыныз: ");
+                yearStart = scanner.nextInt();
+
+                if (yearStart < dateOfBirth || yearStart > LocalDate.now().getYear()) {
+                    throw new InputMismatchException("Жумушка кирген жылыныз " + (dateOfBirth + 18) + " жылдан кичине же 2024жылдан чон болбойт");
+                }
+                int age = LocalDate.now().getYear() - dateOfBirth;
+
+                int experience = LocalDate.now().getYear() - yearStart;
+                if (dateOfBirth+18> yearStart) {
+                    throw new MyException("Иштегенге болбойт");
+                }
+                Person person = new Person(name, lastName, dateOfBirth, yearStart);
+                System.out.println(person);
+
+                System.out.println("Age: " + age);
+                System.out.println("Experience: " + experience);
+            }
+            catch (MyException e) {
+                System.out.println(e.getMessage());
+            }catch (Exception e) {
+                System.err.println(e.getMessage());
+
         }
+
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,62 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+    private String name;
+    private String surname;
+    private int dateOfBirth;
+    private int dateOfStart;
+
+    public Person(String name, String surname, int dateOfBirth, int yearStart) {
+        this.name = name;
+        this.surname = surname;
+        this.dateOfBirth = dateOfBirth;
+        this.dateOfStart = yearStart;
+    }
+    public Person(String name, String surname, int dateOfBirth) {
+        this.name = name;
+        this.surname = surname;
+        this.dateOfBirth = dateOfBirth;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(int dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getDateOfStart() {
+        return dateOfStart;
+    }
+
+    public void setDateOfStart(int dateOfStart) {
+        this.dateOfStart = dateOfStart;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", dateOfStart=" + dateOfStart +
+                '}';
+    }
 }
+
